f1dad81fbf5a147f0405ea18f3f5da3309c70ff2
David <davidobob@laposte.net>
2020-11-17 10:50:44 +0100

    added dots on hover on inputs

--- src/components/common/TextArea.jsx
+++ src/components/common/TextArea.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextArea(props) {
   const { placeholder } = props;
 
+  // Default value for state
+  const defaultSettings =
+    placeholder === "Employé" ? "dot-right hidden" : "dot-left hidden";
+
+  // state
+  const [attribute, setAttribute] = useState(defaultSettings);
+
+  // Remove hidden when hover an input
+  const onMouseEnter = () => {
+    setAttribute(placeholder === "Employé" ? "dot-right" : "dot-left");
+  };
+
+  // Write hidden when exit an input
+  const onMouseOut = () => {
+    setAttribute(
+      placeholder === "Employé" ? "dot-right hidden" : "dot-left hidden"
+    );
+  };
+
   return (
     <div className="draggable">
-      <input className="text-area" type="text" placeholder={placeholder} />
+      <input
+        className="text-area"
+        type="text"
+        placeholder={placeholder}
+        onMouseEnter={onMouseEnter}
+        onMouseOut={onMouseOut}
+      />
+      <div className={attribute}></div>
     </div>
   );
 }
